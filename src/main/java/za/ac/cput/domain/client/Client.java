863ca3c5bd299bb29014a9ecef5d82a24a37c4f9
Tsireledzo Makhado <makhtsire29@gmail.com>
2023-03-06 23:50:56 +0200

    client added

--- src/main/java/za/ac/cput/domain/client/Client.java
+++ src/main/java/za/ac/cput/domain/client/Client.java
@@ -1,2 +1,19 @@
-package za.ac.cput.domain.client;public class Client {
+package za.ac.cput.domain.client;
+
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+    private int port;
+
+    public void connectionToServer() throws Exception{
+        socket = new Socket("localhost",port);
+        System.out.println("connection established in port: "+port);
+    }
+
+    public static void main(String[] args) throws Exception{
+        Client client = new Client();
+        client.connectionToServer();
+    }
+
 }
