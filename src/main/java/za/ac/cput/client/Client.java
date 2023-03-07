1e88b3731d848b83cbf8ac10835ac8462e800e46
Tsireledzo Makhado <makhtsire29@gmail.com>
2023-03-07 13:04:39 +0200

    streams and send Methods done

--- src/main/java/za/ac/cput/client/Client.java
+++ src/main/java/za/ac/cput/client/Client.java
@@ -1,19 +1,31 @@
 package za.ac.cput.client;
 
+import za.ac.cput.domain.JobSeeker;
+
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
     private Socket socket;
     private int port;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
 
     public void connectionToServer() throws Exception{
         socket = new Socket("localhost",port);
         System.out.println("connection established in port: "+port);
     }
 
-    public static void main(String[] args) throws Exception{
-        Client client = new Client();
-        client.connectionToServer();
+    public void streams() throws Exception{
+        inputStream = new ObjectInputStream(socket.getInputStream());
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+        outputStream.flush();
     }
+
+    public void sendMessageToServer(JobSeeker jobSeeker) throws Exception{
+        outputStream.writeObject(jobSeeker);
+    }
+
+
 
 }
